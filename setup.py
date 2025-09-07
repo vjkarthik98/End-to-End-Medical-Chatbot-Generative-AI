6ef3512bcf1765a28ea445cb01819218fb5322c6
vjkarthik98 <karthikvj398@gmail.com>
2025-09-07 12:22:48 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generstive AI Project',
+    Version = '0.0.0',
+    author = 'Vijaya Karthik',
+    author_email= 'karthikvj398@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
